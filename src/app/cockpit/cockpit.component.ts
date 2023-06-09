fd04eeaf8de9e473aab01fc23117478343a2bd67
selenyel <selenyelyazilim@gmail.com>
2023-06-09 09:43:30 +0300

    Making an alias for the @Output Decorator
    @Output and @Input are decorators that marks a class field as an output or an input property and supplies or demands configuration metadata. The DOM property bound to the output and input properties are automatically updated during change detection.
    
    A common pattern in Angular is sharing data between a parent component and one or more child components. Implement this pattern with the @Input() and @Output() decorators.
    
    Yesterday's commets.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,7 @@ export class CockpitComponent implements OnInit {
   newServerContent = '';
 
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent:string}>();
-  @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent:string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent:string}>();
 
   constructor() { }
 
